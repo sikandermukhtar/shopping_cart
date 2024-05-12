53e3ed3ed60fb3fcbf704629782146eafcdc4bff
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-05-12 22:14:34 +0500

    Add Header.jsx that wraps Navbar.jsx and MenuSlider.jsx components.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import {MenuProvider} from "../../contexts/index.js";
+import {useState} from "react";
+import Navbar from "./Navbar.jsx";
+import MenuSlider from "./MenuSlider.jsx";
+
+export default function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen((prev) => !prev);
+
+    };
+    return(
+        <MenuProvider value={{isMenuOpen, toggleMenu}}>
+            <div className='flex'>
+                <Navbar/>
+                <MenuSlider/>
+            </div>
+        </MenuProvider>
+    )
+}
