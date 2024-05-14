7537f7b91638bdf47f017e4235c4298f8bc79333
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-05-15 00:09:46 +0500

    Add routing for login and register pages.

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import './index.css'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
+import LoginPage from "./components/auth/login/LoginPage.jsx";
+import RegisterPage from "./components/auth/register/RegisterPage.jsx";
 
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<Layout/>} >
             <Route path="" element={<Home/>} />
+            <Route path="arch/">
+                <Route path="login/" element={<LoginPage />} />
+                <Route path="register/" element={<RegisterPage />} />
+            </Route>
         </Route>
     )
 )
