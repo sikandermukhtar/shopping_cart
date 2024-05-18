afd83c88d09cc338d3c01c6580f7045e84b4fbb7
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-05-19 00:27:54 +0500

    Improve code.

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
-import Layout from "./Layout.jsx";
-import Home from "./components/Home/Home.jsx";
-import LoginPage from "./components/auth/login/LoginPage.jsx";
-import RegisterPage from "./components/auth/register/RegisterPage.jsx";
-
-
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path='/' element={<Layout/>} >
-            <Route path="" element={<Home/>} />
-            <Route path="arch/">
-                <Route path="login/" element={<LoginPage />} />
-                <Route path="register/" element={<RegisterPage />} />
-            </Route>
-        </Route>
-    )
-)
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <App />
   </React.StrictMode>,
 )
